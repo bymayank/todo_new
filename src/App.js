d8a13115b1d10b424ec743c105e6e66d1eec1a0e
mayank.garg <mayank.garg@innovaccer.com>
2022-05-17 09:11:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import Main from "./components/Home";
+import Navbar from "./components/Navbar";
+import {Input} from "@innovaccer/design-system";
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [mode, setMode] = useState("light");
+  const todoRef = useRef();
+  function toggle() {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#D3D3D3";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  }
+
+  function checkTodo(id) {
+    const newList = [...todo];
+    const todos = newList.find((todos) => todos.id === id);
+    todos.status = !todos.status;
+    setTodo(newList);
+  }
+
+  function addTodo() {
+    const task = todoRef.current.value;
+    let date = new Date();
+    if (task === "") return;
+
+    setTodo((prevTask) => {
+      return [...prevTask, { id: date, task: task, status: false }];
+    });
+    todoRef.current.value = null;
+  }
+  function removeTodo() {
+    const newList = todo.filter((todos) => !todos.status);
+    setTodo(newList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Navbar mode={mode} toggle={toggle} />
+      <div className="container my-2">
+        <Main todos={todo} checkTodo={checkTodo} />
+        <Input ref={todoRef} type="text" />
+        <div
+          class="btn-group"
+          role="group"
+          aria-label="Basic mixed styles example"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <button type="button" class="btn btn-danger" onClick={addTodo}>
+            Add
+          </button>
+          <button type="button" class="btn btn-success" onClick={removeTodo}>
+            Remove
+          </button>
+        </div>
+
+        <div>{todo.filter((todo) => !todo.status).length} Task left</div>
+      </div>
+    </>
   );
 }
 
